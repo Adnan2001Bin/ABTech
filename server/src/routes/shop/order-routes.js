e10afa9f5083a6fc96c169d0cf923b4f7f5891e5
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2025-02-24 18:44:24 +0600

    add

--- server/src/routes/shop/order-routes.js
+++ server/src/routes/shop/order-routes.js
@@ -8,27 +8,27 @@ router.post("/create", createOrder);
 
 // SSLCommerz callback routes
 router.post("/sslcommerz-success", (req, res) => {
-  console.log("Request Body:", req.body); // Log the entire request body
+  console.log("Request Body:", req.body);
   const { tran_id, val_id, paymentId, payerId } = req.body;
   res.redirect(
-    `http://localhost:5173/success?tran_id=${tran_id}&val_id=${val_id}&paymentId=${paymentId}&payerId=${payerId}`
+    `${process.env.CORS_ORIGIN}/success?tran_id=${tran_id}&val_id=${val_id}&paymentId=${paymentId}&payerId=${payerId}`
   );
 });
 
 router.post("/sslcommerz-fail", (req, res) => {
-  const { tran_id } = req.body; // Extract data from req.body
+  const { tran_id } = req.body;
   console.log("Payment failed. Transaction ID:", tran_id);
-  res.redirect("http://localhost:5173/sslcommerz-fail"); // Redirect to frontend fail page
+  res.redirect(`${process.env.CORS_ORIGIN}/sslcommerz-fail`);
 });
 
 router.post("/sslcommerz-cancel", (req, res) => {
-  const { tran_id } = req.body; // Extract data from req.body
+  const { tran_id } = req.body;
   console.log("Payment canceled. Transaction ID:", tran_id);
-  res.redirect("http://localhost:5173/sslcommerz-cancel"); // Redirect to frontend cancel page
+  res.redirect(`${process.env.CORS_ORIGIN}/sslcommerz-cancel`);
 });
 
 router.post("/sslcommerz-ipn", (req, res) => {
-  const paymentData = req.body; // Extract data from req.body
+  const paymentData = req.body;
   console.log("IPN received:", paymentData);
   res.status(200).send("IPN received");
 });
@@ -37,6 +37,5 @@ router.post("/capture", capturePayment);
 
 router.get("/list/:userId", getAllOrdersByUser);
 router.get("/details/:id", getOrderDetails);
-
 
 export default router;
