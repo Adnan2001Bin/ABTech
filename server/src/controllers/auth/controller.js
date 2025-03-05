8fb611147551dc8e98e07de14a2583a21709fe43
Adnan Bin Hossen <hossen15-4543@diu.edu.bd>
2025-03-05 15:05:42 +0600

    commit

--- server/src/controllers/auth/controller.js
+++ server/src/controllers/auth/controller.js
@@ -312,7 +312,7 @@ export const resetPassword = async (req, res) => {
 
 export const isAuthenticated = async (req, res) => {
   try {
-    const token = req.cookies?.token || req.headers.authorization?.split(" ")[1];
+    const token = req.cookies.token;
     if (!token) {
       return res.status(401).json({ success: false, message: "Not authenticated." });
     }
